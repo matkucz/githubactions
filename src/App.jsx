cb8ea62589ca43ae472d87c1e99c2009dc8612a8
Mateusz Kuczyński <mat.kuczyn@gmail.com>
2022-01-21 17:16:16 +0100

    Change in App component

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello simple app
+          Hello simple app - Mateusz Kuczyński
         </p>
         <a
           className="App-link"
